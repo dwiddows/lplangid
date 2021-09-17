6bada6fc05e09d59bd8e1d4e6c8d80e2b3989cd5
Dominic Widdows <dwiddows@liveperson.com>
2021-09-17 15:15:19 -0700

    Niceties to please twine

--- setup.py
+++ setup.py
@@ -5,12 +5,16 @@ setup(
     name='lplangid',  # Required.
     version='0.1.0',  # Required. Use major.minor.dev format.
     description='LivePerson language detection package using a Reciprocal Rank Classifier',  # Required
+    long_description="""This package is a python implementation of the classifier described in the paper 
+"Language Identification with a Reciprocal Rank Classifier".""",
+    long_description_content_type='text/x-rst',
+
     packages=['lplangid', 'training'],
     include_package_data=True,
     data_files=[('lplangid/freq_data', glob.glob('lplangid/freq_data/*.csv'))],
     install_requires=['flake', 'pytest'],  # List new package requirements here, but please be sure you need them!
 
-    url='',  # Optional
+    url='https://github.com/LivePersonInc/lplangid',  # Optional
     author='Dominic Widdows, Chris Brew',  # Optional
     author_email='dwiddows@gmail.com,christopher.brew@gmail.com',   # Optional
     classifiers=[  # Optional
