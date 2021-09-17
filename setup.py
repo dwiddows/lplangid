a204f594b2a7a3449872008680e7e0686eb6b4f6
Dominic Widdows <dwiddows@liveperson.com>
2021-09-17 15:35:55 -0700

    Removed depenencies that aren't needed for running - this makes a clean distribution

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lplangid',  # Required.
-    version='0.1.0',  # Required. Use major.minor.dev format.
+    version='0.1.1',  # Required. Use major.minor.dev format.
     description='LivePerson language detection package using a Reciprocal Rank Classifier',  # Required
     long_description="""This package is a python implementation of the classifier described in the paper 
 "Language Identification with a Reciprocal Rank Classifier".""",
@@ -12,15 +12,12 @@ setup(
     packages=['lplangid', 'training'],
     include_package_data=True,
     data_files=[('lplangid/freq_data', glob.glob('lplangid/freq_data/*.csv'))],
-    install_requires=['flake', 'pytest'],  # List new package requirements here, but please be sure you need them!
+
+    # No new packages are needed for running lplangid - the below are useful for development.
+    # install_requires=['flake', 'pytest'],  # List new package requirements here, but please be sure you need them!
 
     url='https://github.com/LivePersonInc/lplangid',  # Optional
     author='Dominic Widdows, Chris Brew',  # Optional
     author_email='dwiddows@gmail.com,christopher.brew@gmail.com',   # Optional
-    classifiers=[  # Optional
-        'Intended Audience :: NLP developers',
-        'Topic :: Natural Language Processing :: Language Detection',
-        'Programming Language :: Python :: 3.6',
-    ],
     keywords='language detection, language classification, reciprocal rank classifier, lplangid'
 )
