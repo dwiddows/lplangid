e00b23a151dd59d2661523482730aa4cabc06905
Dominic Widdows <widdows@ionq.com>
2023-03-18 13:25:09 -0700

    Updated setup.py with new repo path

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='lplangid',  # Required.
     version='0.1.4',  # Required. Use major.minor.dev format.
-    description='LivePerson language detection package using a Reciprocal Rank Classifier',  # Required
+    description='Language detection package using a Reciprocal Rank Classifier',  # Required
     long_description='This package is a python implementation of the classifier described in the paper '
                      '"Language Identification with a Reciprocal Rank Classifier" (https://arxiv.org/abs/2109.09862).',
     long_description_content_type='text/x-rst',
@@ -16,7 +16,7 @@ setup(
     # No new packages are needed for running lplangid - the below are useful for development.
     # install_requires=['flake', 'pytest'],  # List new package requirements here, but please be sure you need them!
 
-    url='https://github.com/LivePersonInc/lplangid',  # Optional
+    url='https://github.com/dwiddows/lplangid',  # Optional
     author='Dominic Widdows, Chris Brew',  # Optional
     author_email='dwiddows@gmail.com,cbrew@acm.org',  # Optional
     keywords='language detection, language classification, reciprocal rank classifier, lplangid'
