00b3972f8c23574ae1901aeeac91ac11ff4232c3
Dominic Widdows <widdows@ionq.com>
2023-03-20 18:41:09 -0700

    More many-language experiments, and version number bump

--- experiments/bible_eval.py
+++ experiments/bible_eval.py
@@ -2,6 +2,8 @@ import logging
 import numpy as np
 import os
 
+import langid
+
 from lplangid import language_classifier as lc
 from experiments import fasttext_client
 from experiments.classification_report import nullsafe_classification_report
@@ -14,54 +16,62 @@ BIBLE_TXT_ROOT = "/Users/widdows/Data/BibleTexts"
 SUBDIRS = ["full", "train", "test"]
 
 
+def langid_classify(text: str):
+    return langid.classify(text)[0]
+
+
 def run_bible_tests(test_dir, num_trials_per_lang=1000):
     top_languages = {x[:2] for x in os.listdir(lc.FREQ_DATA_DIR)}
-    all_term_ranks, all_char_weights = lc.prepare_scoring_tables(lc.FREQ_DATA_DIR)
-    rrc_classifier = lc.RRCLanguageClassifier(all_term_ranks, all_char_weights)
+    rrc_classifier = lc.RRCLanguageClassifier.many_language_bible_instance()
     ft_classifier = fasttext_client.FastTextLangID()
     filenames = os.listdir(test_dir)
 
-    total_tests, total_attempted, total_correct = 0, 0, 0
-    y_labels, y_pred = [], []
-    for filename in filenames:
-        attempted, correct = 0, 0
-        lang = filename.split(".")[0]
+    fn_labels = [[rrc_classifier.get_winner, "RRC"],
+                 [ft_classifier.predict_lang, "FastText"],
+                 [langid_classify, "LangID"]]
 
-        if lang not in top_languages:
-            continue
+    for fn, label in fn_labels:
+        print(f"Classifying with {label}")
+        total_tests, total_attempted, total_correct = 0, 0, 0
+        y_labels, y_pred = [], []
+        for filename in filenames:
+            attempted, correct = 0, 0
+            lang = filename.split(".")[0]
 
-        available_test_lines = open(os.path.join(test_dir, filename)).readlines()
-        if len(available_test_lines) < num_trials_per_lang:
-            logging.warning(f"Only {len(available_test_lines)} test lines for language {lang} from file {filename}.")
-        test_lines = np.random.choice(available_test_lines, num_trials_per_lang)
+            if lang not in top_languages:
+                continue
 
-        for test_line in test_lines:
-            result = rrc_classifier.get_winner(test_line)
-            result = ft_classifier.predict_lang(test_line)
+            available_test_lines = open(os.path.join(test_dir, filename)).readlines()
+            if len(available_test_lines) < num_trials_per_lang:
+                logging.warning(f"Only {len(available_test_lines)} test lines for language {lang} from file {filename}.")
+            test_lines = np.random.choice(available_test_lines, num_trials_per_lang)
 
-            y_pred.append(result)
-            y_labels.append(lang)
-            if result:
-                attempted += 1
-                if result == lang:
-                    correct += 1
-        if correct == 0:
-            print(f"Skipping missing language {lang}")
-            continue
+            for test_line in test_lines:
+                result = fn(test_line)
+
+                y_pred.append(result)
+                y_labels.append(lang)
+                if result:
+                    attempted += 1
+                    if result == lang:
+                        correct += 1
+            if correct == 0:
+                print(f"Skipping missing language {lang}")
+                continue
+            # print(
+            #     f"Language: {lang} Trials: {len(test_lines)}. Attempted: {attempted}. Correct: {correct}. "
+            #     f"Precision: {correct / attempted:0.3f}. Recall: {correct / len(test_lines)}"
+            # )
+            total_tests += len(test_lines)
+            total_attempted += attempted
+            total_correct += correct
+
         print(
-            f"Language: {lang} Trials: {len(test_lines)}. Attempted: {attempted}. Correct: {correct}. "
-            f"Precision: {correct / attempted:0.3f}. Recall: {correct / len(test_lines)}"
+            f"All languages. Trials: {num_trials_per_lang}. Attempted: {total_attempted}. Correct: {total_correct}. "
+            f"Precision: {total_correct / total_attempted:0.3f}. Recall: {total_correct / total_tests}"
         )
-        total_tests += len(test_lines)
-        total_attempted += attempted
-        total_correct += correct
 
-    print(
-        f"All languages. Trials: {num_trials_per_lang}. Attempted: {total_attempted}. Correct: {total_correct}. "
-        f"Precision: {total_correct / total_attempted:0.3f}. Recall: {total_correct / total_tests}"
-    )
-
-    print(nullsafe_classification_report(y_labels, y_pred))
+        print(nullsafe_classification_report(y_labels, y_pred))
 
 
 def main():
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='lplangid',  # Required.
-    version='1.0.0',    # Required. Use major.minor.dev format.
+    version='1.0.2',    # Required. Use major.minor.dev format.
     description='Language detection package using a Reciprocal Rank Classifier',  # Required
     long_description='This package is a python implementation of the classifier described in the paper '
                      '"Language Identification with a Reciprocal Rank Classifier" (https://arxiv.org/abs/2109.09862).',
@@ -11,7 +11,8 @@ setup(
 
     packages=['lplangid', 'training'],  # Note that 'experiments' is not included.
     include_package_data=True,
-    data_files=[('lplangid/freq_data', glob.glob('lplangid/freq_data/*.csv'))],
+    data_files=[('lplangid/freq_data', glob.glob('lplangid/freq_data/*.csv')),
+                ('lplangid/freq_data_bible', glob.glob('lplangid/freq_data_bible/*.csv'))],
 
     # No new packages are needed for running lplangid - the below are useful for development.
     # install_requires=['flake', 'pytest'],  # List new package requirements here, but please be sure you need them!
